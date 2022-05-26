9890b5fcb59134a95c0b000ac26d6930ed8ecc34
taek <tttck88@gmail.com>
2022-05-26 23:18:03 +0900

    엔티티 클래스 설계

---
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorColumn(name = "A")
+@Getter
+@Setter
+public class Album extends Item {
+
+    private String artist;
+    private String etc;
+}
